b0e4ce5ac36ad77b4cb88e05efcbed6f438cb676
Thomas Green <tho119cl@gmail.com>
2012-07-31 17:56:09 +0100

    Replaced dicts with classes for all currently parsable sections. prospectively changed RoRImporter.py to use the classes

--- source/rorimporter.py
+++ source/rorimporter.py
@@ -5,8 +5,8 @@ from Py3dsMax import mxs
 from RoRImporter import TruckFileReader
 reload(TruckFileReader)
 
-def point3(lst):
-    mxs.Point3(lst[0], lst[1], lst[2])
+#def point3(lst):
+#    mxs.Point3(lst[0], lst[1], lst[2])
 
 class Importer:
     def __init__(self):
@@ -16,22 +16,17 @@ class Importer:
 
     def make_node_beam(self):
         nodes = self.reader.nodes
-        positions = [mxs.Point3(n['x'], n['y'], n['z']) for n in nodes]
+        positions = [mxs.Point3(n.x, n.y, n.z) for n in nodes]
+        
         beams = self.reader.beams
-
 
         beam_object = mxs.SplineShape(pos=mxs.Point3(0,0,0), name="beam_1")
         for beam in beams:
             current_spline = mxs.AddNewSpline(beam_object)
-            mxs.AddKnot(beam_object, current_spline, mxs.pyhelper.namify("corner"), mxs.pyhelper.namify("line"), positions[beam['node1']])
-            mxs.AddKnot(beam_object, current_spline, mxs.pyhelper.namify("corner"), mxs.pyhelper.namify("line"), positions[beam['node2']])
+            mxs.AddKnot(beam_object, current_spline, mxs.pyhelper.namify("corner"), mxs.pyhelper.namify("line"), positions[beam.node1])
+            mxs.AddKnot(beam_object, current_spline, mxs.pyhelper.namify("corner"), mxs.pyhelper.namify("line"), positions[beam.node2])
             mxs.UpdateShape(beam_object)
         mxs.UpdateShape(beam_object)
-
-        #for node in nodes:
-        #    pos = [node['x'], node['y'], node['z']]
-        #    s = mxs.Sphere(radius = 0.1)
-        #    s.pos = mxs.Point3(node['x'], node['y'], node['z'])
 
 class RoRParseError(Exception):
     def __init__(self, value):
